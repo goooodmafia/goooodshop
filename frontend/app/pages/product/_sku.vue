e067cea94f29049954614b4e6df77a21da6d858e
iskovskikh@gmail.com <Iskovskikh Alexey>
2021-03-09 05:32:16 +0300

    commit

--- frontend/app/pages/product/_sku.vue
+++ frontend/app/pages/product/_sku.vue
@@ -1,13 +1,43 @@
 <template>
   <Wrapper>
-    <hr/>
-    <h3>Product</h3>
-    <hr/>
+    <div class="container">
+      <div class="row">
+        <Breadcrumbs :data="breadcrumbs">
+          <template v-slot:sidebar>
+            <div class="col-md-auto col-sm-12">
+              <Sidebar>
+                <!-- <Filters :filters="filters"/>-->
+              </Sidebar>
+            </div>
+          </template>
+
+          <template>
+
+          </template>
+
+        </Breadcrumbs>
+
+      </div>
+    </div>
   </Wrapper>
 </template>
+
 <script>
+
 import Wrapper from "../../components/layout/Wrapper";
+import Breadcrumbs from "~/components/layout/Breadcrumbs";
+import Sidebar from "~/components/category/Sidebar";
+
 export default {
-  components: {Wrapper}
+  components: {Wrapper, Breadcrumbs, Sidebar},
+
+  data() {
+    return {
+      breacrumbs: {
+        title: this.$t('page.help.title'),
+        breadcrumbs: [],
+      },
+    }
+  },
 }
 </script>
